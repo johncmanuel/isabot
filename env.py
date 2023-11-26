cc6ef3fa68408fe92683bdcff268f65d73d127ea
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-25 19:41:05 -0800

    env vars

--- env.py
+++ env.py
@@ -1,19 +1,29 @@
 from os import getenv
+from typing import Any
 
 from dotenv import load_dotenv
 
+
+def handle_getenv(env: str, default: Any = None):
+    """Ensures that the env variables are not None."""
+    value = getenv(env, default)
+    if not value:
+        raise ValueError(f"Environment variable {env} is not set.")
+    return value
+
+
 load_dotenv()
 
-DISCORD_TOKEN = getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = handle_getenv("DISCORD_TOKEN")
 
-DISCORD_APP_ID = getenv("DISCORD_APP_ID")
+DISCORD_APP_ID = handle_getenv("DISCORD_APP_ID")
 
-DISCORD_PUBLIC_KEY = getenv("DISCORD_PUBLIC_KEY")
+DISCORD_PUBLIC_KEY = handle_getenv("DISCORD_PUBLIC_KEY")
 
-DISCORD_WEBHOOK_URL = getenv("DISCORD_WEBHOOK_URL")
+DISCORD_WEBHOOK_URL = handle_getenv("DISCORD_WEBHOOK_URL")
 
-BATTLENET_CLIENT_SECRET = getenv("BATTLENET_CLIENT_SECRET")
+BATTLENET_CLIENT_SECRET = handle_getenv("BATTLENET_CLIENT_SECRET")
 
-BATTLENET_CLIENT_ID = getenv("BATTLENET_CLIENT_ID")
+BATTLENET_CLIENT_ID = handle_getenv("BATTLENET_CLIENT_ID")
 
-PORT = int(getenv("PORT", "8000"))
+PORT = int(handle_getenv("PORT", "8000"))
