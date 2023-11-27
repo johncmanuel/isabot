3197d981d59ac4585fecf263de41594e212b5982
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-26 19:36:18 -0800

    wip

--- isabot/api/api.py
+++ isabot/api/api.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Request, Response
+from fastapi.responses import RedirectResponse
 
-from env import DISCORD_PUBLIC_KEY
-from isabot.discord import verify
+from env import DISCORD_APP_ID, DISCORD_CHANNEL_ID, DISCORD_PUBLIC_KEY
+from isabot.api.discord import invite
+from isabot.api.handlers import handle_discord_app
 
 router = APIRouter()
 
 
 @router.post("/")
-async def create_discord_app(req: Request, res: Response):
-    pass
+async def root(req: Request, res: Response):
+    interaction_res = handle_discord_app(req, DISCORD_PUBLIC_KEY, DISCORD_CHANNEL_ID)
+    return Response(interaction_res)
 
 
 @router.post("/webhook")
@@ -24,3 +27,13 @@ async def automated_webhook():
 @router.post("/register")
 async def register_cmds():
     pass
+
+
+@router.get("/hi")
+async def hi():
+    return {"message": "hi there"}
+
+
+@router.get("/invite")
+async def inv():
+    return RedirectResponse(invite.get_invite_url(DISCORD_APP_ID))
--- isabot/api/handlers.py
+++ isabot/api/handlers.py
@@ -1,13 +1,45 @@
-from fastapi import Request
+from fastapi import Request, Response
 
-from env import DISCORD_PUBLIC_KEY
+from isabot.api.discord import register
 from isabot.discord import verify
+from isabot.discord.discord_types import (
+    APIInteractionType,
+    ApplicationCommandOptionType,
+)
 
 
-async def handle_discord_app(req: Request):
-    if not DISCORD_PUBLIC_KEY:
-        raise Exception("Public key for discord not given")
-    v = await verify.verify_request(req, DISCORD_PUBLIC_KEY)
+async def handle_discord_app(
+    req: Request, discord_public_key: str, discord_channel_id: str
+) -> Response:
+    """
+    Handle incoming requests from Discord
+    https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+    """
+    v = await verify.verify_request(req, discord_public_key)
     body, error = v["body"], v["error"]
     if error:
         return error
+    interactionType: APIInteractionType = body["type"]
+    print("body", body)
+
+    if interactionType != APIInteractionType.ApplicationCommand:
+        return Response("Invalid request.", 400)
+    if body["channel_id"] != discord_channel_id:
+        return Response("Invalid request.", 400)
+    if not body["member"]:
+        return Response("Invalid request.", 400)
+    if not body["member"]["user"]:
+        return Response("Invalid request.", 400)
+    if not body["data"]["options"] or len(body["data"]["options"]) == 0:
+        return Response("Internal server error.", 500)
+
+    o = body["data"]["options"][0]
+    name, cmd_type = o["name"], o["type"]
+    if cmd_type != ApplicationCommandOptionType.SubCommand:
+        print("Invalid option type")
+        return Response("Invalid request.", 400)
+
+    if name == register.REGISTER_NAME:
+        pass
+
+    return Response("Invalid request.", 400)
