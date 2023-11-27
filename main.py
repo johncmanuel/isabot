d332b32ce82615925bf19b8bd17a0b98e8c6c366
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-26 19:01:43 -0800

    main

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from env import DISCORD_APP_ID, DISCORD_TOKEN
 from isabot.api import api
-from isabot.api.discord import register, setup
-from isabot.discord.commands import register_slash_command
+from isabot.api.discord import base
+from isabot.discord import commands
 
-app = FastAPI()
-app.include_router(setup.router)
+
+@asynccontextmanager
+async def setup_app(app: FastAPI):
+    """Setup app by registering the slash commands and other stuff"""
+    await commands.register_slash_command(base.BASE, DISCORD_TOKEN, DISCORD_APP_ID)
+    yield
+    # clean up anything else before exiting app
+    # ...
+
+
+app = FastAPI(lifespan=setup_app)
 app.include_router(api.router)
