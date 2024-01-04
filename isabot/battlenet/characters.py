81bcea7baab5ed6b6188ea34750cfd37aa75b22c
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-01-03 16:15:58 -0800

    endpt

---
+++ isabot/battlenet/characters.py
@@ -0,0 +1,11 @@
+from isabot.battlenet.helpers import get_bnet_endpt
+
+
+async def character_mounts(
+    token: str, character_name: str, realm_slug: str, namespace: str = "profile"
+):
+    return await get_bnet_endpt(
+        f"/profile/wow/character/{realm_slug}/{character_name}/collections/mounts",
+        token,
+        namespace,
+    )
