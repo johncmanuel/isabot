a4417d54d4a8558a19af4c1b8e9afae918abbc09
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-01-05 17:13:09 -0800

    update api routes

--- isabot/api/api.py
+++ isabot/api/api.py
@@ -1,9 +1,11 @@
 # pyright: reportOptionalMemberAccess=false
 
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, BackgroundTasks, Request
 from fastapi.responses import RedirectResponse
 
 import isabot.api.handlers as handlers
+import isabot.api.leaderboards.leaderboard as leaderboard
+import isabot.api.leaderboards.update as update
 import isabot.battlenet.oauth as oauth
 from env import DISCORD_APP_ID, DISCORD_CHANNEL_ID, DISCORD_PUBLIC_KEY
 
@@ -19,8 +21,17 @@ async def root(request: Request):
     return interaction_res
 
 
-@router.post("/webhook")
-async def manual_webhook():
+@router.get("/webhook")
+async def manual_webhook(background_tasks: BackgroundTasks):
+    # t = (await oauth.cc_get_access_token())["access_token"]
+    # l = leaderboard.Leaderboard(t)
+    # b = await l.get_normal_bg_stats()
+    # m = await l.get_mounts()
+    # g = await l.get_users()
+    # entry = l.create_entry(g, m, b)
+    # await l.upload_entry(entry)
+
+    # return {"details": "updating the database now..."}
     pass
 
 
@@ -56,6 +67,6 @@ async def test():
 
 
 @router.get("/update")
-async def update_leaderboard(request: Request):
+async def update_leaderboard(request: Request, background_tasks: BackgroundTasks):
     """https://stackoverflow.com/questions/53181297/verify-http-request-from-google-cloud-scheduler"""
-    return await handlers.handle_update_leaderboard(request)
+    return await handlers.handle_update_leaderboard(request, background_tasks)
