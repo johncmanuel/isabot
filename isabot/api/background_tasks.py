c1f3763786ebf06bf5f8dafa1f5c9d64e51354a0
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-01-17 19:18:30 -0800

    use pydantic model typings

--- isabot/api/background_tasks.py
+++ isabot/api/background_tasks.py
@@ -1,6 +1,5 @@
 import asyncio
 from datetime import datetime
-from enum import IntEnum
 from typing import Optional
 
 import isabot.api.leaderboards.leaderboard as leaderboard
@@ -8,6 +7,7 @@ import isabot.api.leaderboards.update as update
 import isabot.discord.webhook as webhook
 import isabot.utils.concurrency as concurr
 from env import DISCORD_WEBHOOK_URL
+from isabot.discord.discord_types import Embed, EmbedColorCodes, EmbedField
 
 
 async def update_db_and_upload_entry(cc_access_token: str, auth_url: str) -> None:
@@ -36,16 +36,29 @@ async def update_db_and_upload_entry(cc_access_token: str, auth_url: str) -> Non
     embeds = []
 
     def prepare_embed(lb_type: str):
-        options = {
-            "title": f"AR Club Leaderboard - {lb_type.replace('_', ' ').title()}",
-            "description": "Weekly WoW guild leaderboard for the AR Club!",
-            "auth_url": auth_url,
-            "lb_type": lb_type,
-            "start_date": datetime.fromtimestamp(entry.date_created).strftime("%c"),
-            "scores": lb.format_entry(entry, lb_type),
-        }
-        embed = create_embed(options)
-        embeds.append({"embeds": embed})
+        scores = lb.format_entry(entry, lb_type)
+        start_date = datetime.fromtimestamp(entry.date_created).strftime("%c")
+        embed = Embed(
+            title=f"AR Club Leaderboard - {lb_type.replace('_', ' ').title()}",
+            description="Weekly WoW guild leaderboard for the AR Club!",
+            auth_url=auth_url,
+            lb_type=lb_type,
+            start_date=start_date,
+            color=get_color_by_leaderboard_type(lb_type),
+            scores=scores,
+            fields=[
+                EmbedField(
+                    name=f"Register your Battle Net account down below:",
+                    value=f"[Click me!]({auth_url})",
+                ),
+                EmbedField(
+                    name=f"Leaderboard for week of {start_date}",
+                    value=f"```{scores}```",
+                    inline=True,
+                ),
+            ],
+        ).model_dump()
+        embeds.append({"embeds": [embed]})
 
     # Run the webhook operations in parallel
     try:
@@ -58,47 +71,10 @@ async def update_db_and_upload_entry(cc_access_token: str, auth_url: str) -> Non
         return
 
 
-def create_embed(options: dict):
-    """
-    https://discord.com/developers/docs/resources/channel#embed-object
-    TODO: Create a type for options; it'll make the dev experience a bit
-    better.
-    """
-    embed = {
-        "title": options["title"],
-        "description": options["description"],
-        "color": get_color_by_leaderboard_type(options["lb_type"]),
-        "fields": [
-            {
-                "name": f"Register your Battle Net account down below:",
-                "value": f"[Click me!]({options['auth_url']})",
-            },
-            {
-                "name": f"Leaderboard for week of {options['start_date']}",
-                "value": f"```{options['scores']}```",
-                "inline": True,
-            },
-        ],
-    }
-
-    return [embed]
-
-
-def get_color_by_leaderboard_type(lb_type: Optional[str] = None):
+def get_color_by_leaderboard_type(lb_type: Optional[str] = None) -> int:
     """lb_type accepts 'mounts' or 'pvp'. If lb_type is invalid or unspecified, default color is darker gray."""
     if lb_type == "mounts":
         return EmbedColorCodes.DARK_GREEN
     elif lb_type == "normal_bg_wins":
         return EmbedColorCodes.DARK_ORANGE
     return EmbedColorCodes.DARKER_GRAY
-
-
-class EmbedColorCodes(IntEnum):
-    """
-    Will only cherry-pick the colors that are needed.
-    https://discordpy.readthedocs.io/en/latest/api.html#colour
-    """
-
-    DARK_GREEN = 0x1F8B4C
-    DARK_ORANGE = 0xA84300  # The closest to brown we can get... :(
-    DARKER_GRAY = 0x546E7A
