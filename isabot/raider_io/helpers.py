020251c9f0681a5dab870b014ef5c8060d94842a
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-02-05 20:06:41 -0800

    setup funcs for raider io api

--- isabot/raider_io/helpers.py
+++ isabot/raider_io/helpers.py
@@ -7,9 +7,27 @@ https://support.raider.io/kb/frequently-asked-questions
 """
 
 
-async def get_raider_io_endpt(url: str, base_url: str = RAIDER_IO_URL):
+async def get_raider_io_endpt(
+    url: str,
+    base_url: str = RAIDER_IO_URL,
+    **params,
+):
+    """
+    Make a GET request to the Raider IO API.
+
+    Example "url" values:
+    - "/characters/profile"
+    - "/periods"
+    - "/guilds/profile"
+
+    Supply additional query parameters using kwargs.
+    """
     try:
-        r = await http_client.get(url=f"{base_url}{url}")
+        r = await http_client.get(
+            url=f"{base_url}{url}",
+            params=params,
+            headers={"Accept": "application/json"},
+        )
         return await r.json()
     except Exception as err:
         print("exception:", err)
---
+++ isabot/raider_io/mythic.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from isabot.raider_io.helpers import get_raider_io_endpt
+from isabot.utils.dictionary import safe_nested_get
+
+
+async def get_character_info(
+    char_name: str, realm: str, region: str, fields: Optional[str]
+):
+    """
+    Get the mythic plus scores for a character.
+    Fields must be a comma-separated string of fields to include in the response.
+    https://raider.io/api#/character/getApiV1CharactersProfile
+    """
+    url = "/characters/profile"
+    if not fields:
+        return await get_raider_io_endpt(
+            url=url,
+            region=region,
+            realm=realm,
+            name=char_name,
+        )
+    fields_cleaned = fields.strip().replace(" ", "")
+    return await get_raider_io_endpt(
+        url=url,
+        region=region,
+        realm=realm,
+        name=char_name,
+        fields=fields_cleaned,
+    )
+
+
+def get_overall_mythic_plus_score(raider_io_character_res: dict) -> float:
+    """
+    Get the overall mythic plus score for a character.
+    """
+    scores = raider_io_character_res.get("mythic_plus_scores_by_season", [])
+    if not scores:
+        return 0
+    return safe_nested_get(scores[0], "scores", "all", default=0)  # type: ignore
